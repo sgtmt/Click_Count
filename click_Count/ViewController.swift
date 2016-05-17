9406fb4712ee7b4601682c1413ab7aea8338e320
sgtmt <ainnuen@gmail.com>
2016-05-18 03:52:07 +0900

    2016/05/18 storyboard count 部分実装　説明追加

--- click_Count/ViewController.swift
+++ click_Count/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+     var a:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        
+    }
+    @IBOutlet weak var count_number: UILabel!
+    
+    func downColor(){
+        if a<=0 {
+            count_number.textColor = UIColor.redColor()
+        }else{
+            count_number.textColor = UIColor.blackColor()
+        }
+    }
+    func ten_count(){
+        if a%10==0 {
+            count_number.textColor = UIColor.blueColor()
+        }else{
+            count_number.textColor = UIColor.blackColor()
+        }
     }
 
+    @IBAction func count_plus(sender: AnyObject) {
+        a+=1
+        ten_count()
+        count_number.text = String(a)
+    }
+    @IBAction func count_down(sender: AnyObject) {
+        a-=1
+        downColor()
+        count_number.text = String(a)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
