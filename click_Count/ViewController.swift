638623bc0ebc59349df7ae363445e55d34c8e125
sgtmt <ainnuen@gmail.com>
2016-05-19 18:13:41 +0900

    2016/05/19 plus sud clear調整

--- click_Count/ViewController.swift
+++ click_Count/ViewController.swift
@@ -21,27 +21,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var count_number: UILabel!
     
     func downColor(){
-        if a<=0 {
+        if  a < 0 {
             count_number.textColor = UIColor.redColor()
         }else{
             count_number.textColor = UIColor.blackColor()
         }
     }
     func ten_count(){
-        if a%10==0 {
+        if a>=1 && a%10==0  {
             count_number.textColor = UIColor.blueColor()
         }else{
             count_number.textColor = UIColor.blackColor()
         }
     }
+    func backBtn_Set(){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func backBtn(sender: AnyObject) {
+        backBtn_Set()
+    }
     func result_event(){
         count_number.text = String(a)
 
     }
+    @IBAction func count_backBtn(sender: AnyObject) {
+        backBtn_Set()
+    }
 
+    @IBAction func reset_Btn(sender: AnyObject) {
+        a=0
+        ten_count()
+        result_event()
+    }
     @IBAction func count_plus(sender: AnyObject) {
         a+=1
-        ten_count()
+        if a<0 {
+            downColor()
+        }else{
+             ten_count()
+        }
+       
         result_event()
             }
     @IBAction func count_down(sender: AnyObject) {
